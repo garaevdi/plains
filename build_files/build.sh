ff7cbf0424616135e141028a11af71ce9bb48bbf
Denis Garaev <garaevdi@yandex.ru>
2025-08-26 14:45:36 +0300

    Add more packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,7 @@ set -ouex pipefail
 dnf5 -y install @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 dnf5 -y install libva-nvidia-driver
 
-dnf5 -y install gnome-text-editor gnome-calculator gnome-clocks baobab gnome-contacts simple-scan papers loupe showtime decibels gstreamer1-plugins-ugly gstreamer1-plugins-bad-free-extras gstreamer1-plugins-good-extras adw-gtk3-theme
+dnf5 -y install gnome-text-editor gnome-calculator gnome-clocks baobab gnome-contacts simple-scan papers loupe showtime decibels file-roller python3-pygit2 python3-dbus nautilus-python gstreamer1-plugins-ugly gstreamer1-plugins-bad-free-extras gstreamer1-plugins-good-extras adw-gtk3-theme
 
 dnf5 -y install --setopt=install_weak_deps=false gnome-calendar
 
