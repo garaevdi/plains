a6f4a840b5fcdc677297be754d8e85381c9e39c4
Denis Garaev <garaevdi@yandex.ru>
2025-08-31 03:02:44 +0300

    Add sing-box and pop-launcher

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,13 +13,17 @@ set -ouex pipefail
 dnf5 -y install @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 dnf5 -y install libva-nvidia-driver
 
-dnf5 -y install gnome-text-editor gnome-calculator gnome-clocks baobab gnome-contacts simple-scan papers loupe showtime decibels file-roller python3-pygit2 python3-dbus nautilus-python gstreamer1-plugins-ugly gstreamer1-plugins-bad-free-extras gstreamer1-plugins-good-extras adw-gtk3-theme steam-devices
+dnf5 -y install gnome-text-editor gnome-calculator gnome-clocks baobab gnome-contacts simple-scan papers loupe showtime decibels file-roller python3-pygit2 python3-dbus nautilus-python gstreamer1-plugins-ugly gstreamer1-plugins-bad-free-extras gstreamer1-plugins-good-extras adw-gtk3-theme steam-devices pop-launcher
 
 dnf5 -y install --setopt=install_weak_deps=false gnome-calendar nextcloud-client
 
 dnf5 -y copr enable trixieua/morewaita-icon-theme
 dnf5 -y install morewaita-icon-theme
 dnf5 -y copr disable trixieua/morewaita-icon-theme
+
+dnf5 -y config-manager addrepo --from-repofile=https://sing-box.app/sing-box.repo
+dnf5 -y install sing-box
+dnf5 -y config-manager setopt sing-box.enabled=0
 
 # Use a COPR Example:
 #
