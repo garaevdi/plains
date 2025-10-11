a7efc1b4b94e22ddc23d9a360f2d4c3068036daf
Denis Garaev <garaevdi@yandex.ru>
2025-10-12 00:21:51 +0300

    Add klassy

--- build_files/build.sh
+++ build_files/build.sh
@@ -42,6 +42,10 @@ dnf5 -y copr enable deltacopy/darkly
 dnf5 -y install darkly
 dnf5 -y copr disable deltacopy/darkly 
 
+dnf5 -y config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:paul4us/Fedora_42/home:paul4us.repo
+dnf5 -y install --setopt=install_weak_deps=false klassy
+dnf5 -y config-manager setopt home_paul4us.enabled=0
+
 dnf5 -y remove firefox nvtop htop supergfxctl kdebugsettings fcitx5 ibus
 
 # Use a COPR Example:
