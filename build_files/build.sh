fe278253fcc34ae3966627e60a6870a2daa0b1c7
Denis Garaev <garaevdi@yandex.ru>
2025-10-13 23:47:05 +0300

    Screw it, we're going niri again

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,46 +14,23 @@ dnf5 -y install @multimedia --setopt="install_weak_deps=False" --exclude=Package
 # dnf5 -y install libva-nvidia-driver
 
 dnf5 -y install \
-    okular \
-    gwenview \
-    kcalc \
-    ksystemlog \
-    elisa-player \
-    merkuro \
+    nautilus \
+    ptyxis \
+    gnome-software \
     gstreamer1-plugins-ugly \
     gstreamer1-plugins-bad-free-extras \
     gstreamer1-plugins-good-extras \
     adw-gtk3-theme \
     steam-devices \
-    pop-launcher \
 	fish
     
-# dnf5 -y install --setopt=install_weak_deps=false gnome-calendar
+dnf5 -y install --setopt=install_weak_deps=false niri fuzzel
 
-# dnf5 -y copr enable trixieua/morewaita-icon-theme
-# dnf5 -y install morewaita-icon-theme
-# dnf5 -y copr disable trixieua/morewaita-icon-theme
+dnf5 -y copr enable trixieua/morewaita-icon-theme
+dnf5 -y install morewaita-icon-theme
+dnf5 -y copr disable trixieua/morewaita-icon-theme
 
-dnf5 -y copr enable matinlotfali/KDE-Rounded-Corners
-dnf5 -y install kwin-effect-roundcorners
-dnf5 -y copr disable matinlotfali/KDE-Rounded-Corners
-
-dnf5 -y copr enable deltacopy/darkly 
-dnf5 -y install darkly
-dnf5 -y copr disable deltacopy/darkly 
-
-dnf5 -y config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:paul4us/Fedora_42/home:paul4us.repo
-dnf5 -y install --setopt=install_weak_deps=false klassy
-dnf5 -y config-manager setopt home_paul4us.enabled=0
-
-dnf5 -y remove firefox nvtop htop supergfxctl kdebugsettings fcitx5 ibus
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y remove firefox nvtop htop
 
 #### Example for enabling a System Unit File
 
